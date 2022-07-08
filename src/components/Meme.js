b2677e25d9f1ce85df80e851dc2299d2e071f55c
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-07-08 13:07:09 +0530

    main

---
+++ src/components/Meme.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export default function Meme(){
+    return(
+        <main>
+            <form className="form">
+                <input 
+                className="form--input" 
+                type="text"
+                placeholder="Top text"
+                />
+                <input 
+                className="form--input" 
+                type="text"
+                placeholder="Bottom text"
+                />
+                <button className="form--button">Get a new image</button>
+            </form>
+        </main>
+    )
+}
