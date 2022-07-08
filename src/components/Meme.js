7702737769ceec636af921ee46f0b058f8698216
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-07-08 13:39:13 +0530

    commit

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,22 +1,39 @@
 import React from "react";
+import memesData from "../memesData.js"
 
+export default function Meme() {
 
-export default function Meme(){
-    return(
+    let url 
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
+    return (
         <main>
-            <form className="form">
-                <input 
-                className="form--input" 
-                type="text"
-                placeholder="Top text"
+            <div className="form">
+            
+                <input
+                    className="form--input"
+                    type="text"
+                    placeholder="Top text"
                 />
-                <input 
-                className="form--input" 
-                type="text"
-                placeholder="Bottom text"
+                <input
+                    className="form--input"
+                    type="text"
+                    placeholder="Bottom text"
                 />
-                <button className="form--button">Get a new image</button>
-            </form>
+                <button className="form--button"
+                    onClick={getMemeImage}
+                >Get a new image</button>
+
+
+
+
+
+            </div>
         </main>
     )
 }
