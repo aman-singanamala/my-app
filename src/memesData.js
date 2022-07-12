3fb05f55a27b1cfe4e1ba1161c145a3dfbb477e7
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-07-12 19:45:09 +0530

    commit

--- src/memesData.js
+++ src/memesData.js
@@ -793,14 +793,6 @@ export default {
                 "width": 698,
                 "height": 900,
                 "box_count": 2
-            },
-            {
-                "id": "100947",
-                "name": "Matrix Morpheus",
-                "url": "https://i.imgflip.com/25w3.jpg",
-                "width": 500,
-                "height": 303,
-                "box_count": 2
             }
         ]
     }
